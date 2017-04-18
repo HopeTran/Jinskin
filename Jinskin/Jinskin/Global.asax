f916da5a680e64e44aa6f938c572e6cf60b21390
Trần Thị Thanh Hồng <hongttt@tinhvan.com>
2017-04-18 15:45:09 +0700

    [U] Update source code

---
+++ Jinskin/Jinskin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jinskin.MvcApplication" Language="C#" %>
---
+++ Jinskin/Jinskin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jinskin.Startup))]
+namespace Jinskin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
